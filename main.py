b592ebd46fd0601468f4d6458f1a46bd59931f9d
Igor Kiryakov <70815675+bigboiii300@users.noreply.github.com>
2022-09-09 12:44:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,6 @@ from pyunpack import Archive
 import pandas as pd
 from win32com import client as wc
 
-# pip3 install lxml
-# создать один единый документ для всей инфы. при нахождении информации создавать
-# гиперссылку на это место и по клику на нее открывать этот момент
 
 hdr = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                      'Chrome/55.0.2883.87 Safari/537.36'}
